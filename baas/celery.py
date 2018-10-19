e106bf65990a1d151805c33b9fd6e20605291fde
Bennaiii <benjamin.parske@gmail.com>
2018-10-19 11:57:28 +0200

    celery baas-app prepared

---
+++ baas/celery.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from celery import Celery
+
+baas_broker = 'amqp://acc13_user:acc13_pw@localhost/acc13_vhost'
+
+app = Celery('baas_celery',
+             broker= baas_broker,
+             backend= baas_broker,
+             include=['baas_celery.tasks'])

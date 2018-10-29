b434d9e0a025f406e8ca4bce1394c70b258d114b
Tristan <thrawst88@gmail.com>
2018-10-29 17:07:27 +0000

    run flower image in detached mode

--- flower/setup.sh
+++ flower/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull iserko/docker-celery-flower
-docker run -ti -p 5555:5555 \
+docker run --rm -d -p 5555:5555 \
   -e CELERY_BROKER_URL=amqp://ubuntu:1234@192.168.1.75:5672/myvhost \
   iserko/docker-celery-flower --broker_api=http://ubuntu:1234@192.168.1.75:5672/myvhost
